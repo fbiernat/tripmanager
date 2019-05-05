b8bfa4141cedd7ae069b9c22d0a7724db0b599c6
fbiernat <filipbiernat@gmail.com>
2019-05-05 15:51:35 +0200

    Implement photos addition method with test

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -1,9 +1,14 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Trip {
 	private String name;
 	private String description;
-	
+
+	private ArrayList<Photo> photos = new ArrayList<>();
+
 	public Trip(String name, String description) {
 		this.name = name;
 		this.description = description;
@@ -23,5 +28,17 @@ public class Trip {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Collection<Photo> getPhotos() {
+		return this.photos;
+	}
+
+	public void addPhoto(Photo photo) throws PhotoAlreadyExistsException{
+		if (photos.contains(photo)) {
+			throw new PhotoAlreadyExistsException();
+		} else {
+			photos.add(photo);
+		}
 	}
 }
--- src/test/java/pl/edu/agh/mwo/TripTest.java
+++ src/test/java/pl/edu/agh/mwo/TripTest.java
@@ -1,9 +1,21 @@
 package pl.edu.agh.mwo;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TripTest {
+
+	Trip trip;
+	TripManager tripManager;
+	Photo photo;
+
+	@Before
+	public void setup() {
+		trip = new Trip("nazwa", "opis");
+		tripManager = new TripManager();
+		photo = new Photo("photo of a cat");
+	}
 
 	@Test
 	public void testConstructor() {
@@ -12,4 +24,16 @@ public class TripTest {
 		assertEquals("opis", trip.getDescription());
 	}
 
+	@Test
+	public void testAddPhoto() throws Exception {
+		trip.addPhoto(photo);
+		assertEquals(1, trip.getPhotos().size());
+	}
+
+	@Test(expected = PhotoAlreadyExistsException.class)
+	public void testAddPhotoTwice() throws Exception {
+		trip.addPhoto(photo);
+		trip.addPhoto(photo);
+		assertEquals(1, trip.getPhotos().size());
+	}
 }
