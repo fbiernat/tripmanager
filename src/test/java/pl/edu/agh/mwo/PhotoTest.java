bbce6a9207a9836400b02c73e0c58d9b28fa2599
fbiernat <filipbiernat@gmail.com>
2019-05-05 16:11:21 +0200

    Implement Photo.java tests

--- src/test/java/pl/edu/agh/mwo/PhotoTest.java
+++ src/test/java/pl/edu/agh/mwo/PhotoTest.java
@@ -1,4 +1,35 @@
 package pl.edu.agh.mwo;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class PhotoTest {
+
+    Photo photo;
+
+    @Before
+    public void setup() {
+        photo = new Photo("photo of a cat");
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        Photo untitledPhoto = new Photo();
+        assertEquals("untitled", untitledPhoto.getComment());
+    }
+
+    @Test
+    public void testConstructorAndCommentGetter() {
+        assertEquals("photo of a cat", photo.getComment());
+    }
+
+    @Test
+    public void testCommentSetter() {
+        photo.setComment("newcomment");
+        assertEquals("newcomment", photo.getComment());
+    }
+
 }
+
